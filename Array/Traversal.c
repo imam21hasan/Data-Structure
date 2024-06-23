fe99cedfc06d888acf02e6289d3a856cbc34f5ec
Imam Hasan Shawon <152065554+IHS-BinaryBird@users.noreply.github.com>
2024-06-24 00:04:12 +0600

    Add files via upload

---
+++ Array/Traversal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i, arr[5] = {10, 20, 30, 40, 50};
+
+    printf("\nThe array is : ");
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+
+    return 0;
+}
