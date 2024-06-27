9c96eac13bc268cff13a62999b5eac3e1dde621a
Imam Hasan Shawon <152065554+IHS-BinaryBird@users.noreply.github.com>
2024-06-27 16:51:22 +0600

    Update and rename TraverseCircularList.c to TraverseDoubleCircularList.c

--- Circuler Linked List/Doubly Linked list/TraverseDoubleCircularList.c
+++ Circuler Linked List/Doubly Linked list/TraverseDoubleCircularList.c
@@ -3,10 +3,10 @@
 struct Node
 {
     int data;
+    struct Node *pre;
     struct Node *next;
 } *head;
 typedef struct Node node;
-
 void traverse()
 {
     node *temp = head;
@@ -15,7 +15,6 @@ void traverse()
         printf("%d  ", temp->data);
         temp = temp->next;
     } while (temp != head);
-    
 }
 int main()
 {
@@ -24,18 +23,24 @@ int main()
     node *b = (node *)malloc(sizeof(node));
     node *c = (node *)malloc(sizeof(node));
 
+    head->pre = head;
     head->data = 10;
     head->next = a;
 
+    a->pre = head;
     a->data = 20;
     a->next = b;
 
+    b->pre = a;
     b->data = 30;
     b->next = c;
 
+    c->pre = b;
     c->data = 40;
     c->next = head;
 
     printf("The linked list is : ");
     traverse();
+
+    return 0;
 }
