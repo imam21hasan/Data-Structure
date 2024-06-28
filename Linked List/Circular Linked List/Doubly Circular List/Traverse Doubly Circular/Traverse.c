2a3d150e31bd0325b4051df578037ce1773ed949
Imam Hasan Shawon <152065554+IHS-BinaryBird@users.noreply.github.com>
2024-06-28 15:11:55 +0600

    Create Traverse.c

---
+++ Linked List/Circular Linked List/Doubly Circular List/Traverse Doubly Circular/Traverse.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *pre;
+    struct Node *next;
+} *head;
+typedef struct Node node;
+void traverse()
+{
+    node *temp = head;
+    do
+    {
+        printf("%d  ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+int main()
+{
+    head = (node *)malloc(sizeof(node));
+    node *a = (node *)malloc(sizeof(node));
+    node *b = (node *)malloc(sizeof(node));
+    node *c = (node *)malloc(sizeof(node));
+
+    head->pre = c;
+    head->data = 10;
+    head->next = a;
+
+    a->pre = head;
+    a->data = 20;
+    a->next = b;
+
+    b->pre = a;
+    b->data = 30;
+    b->next = c;
+
+    c->pre = b;
+    c->data = 40;
+    c->next = head;
+
+    printf("The doubly circular linked list is : ");
+    traverse();
+
+    return 0;
+}
