59394b29650e8075809d4c1b27ea3238468dcd90
Imam Hasan Shawon <152065554+IHS-BinaryBird@users.noreply.github.com>
2024-06-28 15:02:00 +0600

    Create Traverse.c

---
+++ Linked List/Circular Linked List/Singly Circular List/Traverse Singly Circular/Traverse.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+} *head;
+typedef struct Node node;
+
+void traverse()
+{
+    node *temp = head;
+    do
+    {
+        printf("%d  ", temp->data);
+        temp = temp->next;
+    } while (temp!=head);  
+}
+int main()
+{
+    head = (node *)malloc(sizeof(node));
+    node *a = (node *)malloc(sizeof(node));
+    node *b = (node *)malloc(sizeof(node));
+    node *c = (node *)malloc(sizeof(node));
+
+    head->data = 10;
+    head->next = a;
+
+    a->data = 20;
+    a->next = b;
+
+    b->data = 30;
+    b->next = c;
+
+    c->data = 40;
+    c->next = head;
+
+    printf("The singly circular linked list is : ");
+    traverse();
+}
