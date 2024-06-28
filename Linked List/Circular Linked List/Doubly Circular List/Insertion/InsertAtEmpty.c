8b8862f66129f411961c4ec3703b8f31dfb757e1
Imam Hasan Shawon <152065554+IHS-BinaryBird@users.noreply.github.com>
2024-06-28 15:14:35 +0600

    Create InsertAtEmpty.c

---
+++ Linked List/Circular Linked List/Doubly Circular List/Insertion/InsertAtEmpty.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *pre;
+    struct Node *next;
+} *head;
+typedef struct Node node;
+void insert(int item)
+{
+    node *new = (node *)malloc(sizeof(node));
+    new->data = item;
+    if (head == NULL)
+    {
+        new->pre = new;
+        new->next = new;
+
+        head = new;
+    }
+}
+void traverse()
+{
+    node *temp = head;
+    do
+    {
+        printf("%d  ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+int main()
+{
+    insert(55);
+
+    printf("The linked list is : ");
+    traverse();
+
+    return 0;
+}
