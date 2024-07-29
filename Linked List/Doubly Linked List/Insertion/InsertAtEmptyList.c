8a6039ccf7191202df0953d7238faccfc76461a7
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2024-07-29 11:15:38 +0600

    Update InsertAtEmptyList.c

--- Linked List/Doubly Linked List/Insertion/InsertAtEmptyList.c
+++ Linked List/Doubly Linked List/Insertion/InsertAtEmptyList.c
@@ -11,6 +11,11 @@ typedef struct Node node;
 void insertEmptyList(int item)
 {
     node *new = (node *)malloc(sizeof(node));
+    if (new == NULL)
+    {
+        printf("Overflow.");
+        return;
+    }
     new->pre = NULL;
     new->data = item;
     new->next = NULL;
